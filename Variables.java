f280501dadc001da01e0959b88b6f68cae8a2031
Sarvesh Sharma <71933941+Sarvesh1786@users.noreply.github.com>
2023-07-29 09:06:46 +0000

    varaiables

---
+++ Variables.java
@@ -0,0 +1,43 @@
+class variables{
+
+
+
+ // Declear instans variables 
+
+
+
+public  String  name ;    // Declear instans variables 
+public int i ;
+public Intger FBD ;
+
+
+
+// Default Constractor
+public faridabad (){
+
+    // Default Constructor
+   // initializing Instance Variable
+
+this.name = " shaktiman" ;
+
+}
+
+// Main Method
+public static void main (String[] args){
+    // Main Method
+
+
+   FBD name  = new FBD () ;  // Object Creation 
+
+
+   // Displaying O/Print
+
+   System.out.println("it's  is your " + name );
+   System.out.println("Defaul value in interis "  + name.I);
+
+
+
+
+}
+
+}
